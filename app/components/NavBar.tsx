6e5f2d37c689dcc62a85e7a129891654d30509cb
koppai <psthu.koppai@gmail.com>
2023-12-11 15:46:49 +0630

    Add classes subjecsts and profile links in NavBar

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -4,9 +4,18 @@ import React from 'react';
 
 const token: string | null = localStorage.getItem('mathilda');
 const NavBar = () => (
-  <div className={`${token ? 'block' : 'hidden'}`}>
+  <div className={`${token ? 'block' : 'hidden'} flex justify-between`}>
     <Link href="/subjects">Subjects</Link>
-    <Link href="/subjects">Subjects</Link>
+    <Link href="/classes">Classes</Link>
+    <Link href="/me">My Profile</Link>
+    <button
+      onClick={() => {
+        localStorage.removeItem('mathilda');
+        window.location.reload();
+      }}
+    >
+      Logout
+    </button>
   </div>
 );
 
