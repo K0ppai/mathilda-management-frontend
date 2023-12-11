2851b899d6ba033658e63d82fcd1dbdc46ed24ca
koppai <psthu.koppai@gmail.com>
2023-12-11 23:52:51 +0630

    Render navbar conditionally

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,26 +1,27 @@
-'use client';
-
 import Link from 'next/link';
 import React from 'react';
-import { deleteCookie, getCookie } from 'cookies-next';
+import { getCookie } from 'cookies-next';
+import LogoutBtn from './LogoutBtn';
+import { cookies } from 'next/headers';
 
 const NavBar = () => {
-  const cookie = getCookie('mathilda');
+  const cookie = getCookie('mathilda', { cookies });
 
   return (
-    <div className={`flex justify-between`}>
+    <div className="flex justify-around w-full py-2">
       <Link href="/subjects">Subjects</Link>
       <Link href="/classes">Classes</Link>
-      <Link href="/me">My Profile</Link>
-      <Link href="/session/new">Login</Link>
-      <Link
-        href="/session/new"
-        onClick={() => {
-          deleteCookie('mathilda');
-        }}
-      >
-        Logout
-      </Link>
+      {cookie ? (
+        <>
+          <Link href="/me">My Profile</Link>
+          <LogoutBtn />
+        </>
+      ) : (
+        <>
+          <Link href="/session/new">Login</Link>
+          <Link href="/">Sign Up</Link>
+        </>
+      )}
     </div>
   );
 };
